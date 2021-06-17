f5762a4192c583b2f87b1a8c1fa62c2c13321f5b
mvw21 <mvw@abv.bg>
2021-06-17 20:39:53 +0300

    Initial commit

---
+++ src/main/java/com/example/demoasdf/student/StudentController.java
@@ -0,0 +1,32 @@
+package com.example.demoasdf.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> students = Arrays.asList(
+            new Student(1,"Maria"),
+            new Student(2,"NotMaria"),
+            new Student(3,"Smith")
+    );
+
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable Integer studentId){
+        return students.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student" + studentId + "does not exist"));
+    }
+
+
+}
---
+++ src/test/java/com/example/demoasdf/DemoasdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoasdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoasdfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
